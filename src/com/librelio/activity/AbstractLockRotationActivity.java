8f2df26301a79ba09476c8933ed6421eeccd52b0
Gusarov Roman <gusarov@netcook.org>
2013-02-22 15:34:57 +0700

    Resolved rotation issue for all needed screens. Started implementation for advertising screen

--- src/com/librelio/activity/AbstractLockRotationActivity.java
+++ src/com/librelio/activity/AbstractLockRotationActivity.java
@@ -1,27 +1,17 @@
 package com.librelio.activity;
 
+import android.os.Bundle;
+
 import com.librelio.base.BaseActivity;
+import com.librelio.utils.SystemHelper;
 
 public abstract class AbstractLockRotationActivity extends BaseActivity{
-	private boolean rotationWasDisabled = false;
 
 	@Override
-	protected void onResume() {
-		int rotationEnable = android.provider.Settings.System.getInt(
-				getContentResolver(), android.provider.Settings.System.ACCELEROMETER_ROTATION, 1);
-		if(rotationEnable == 0){
-			rotationWasDisabled = true;
-		} else {
-			enableRotation(false);
-		}
-		super.onResume();
-	}
-
-	@Override
-	protected void onPause() {
-		if(!rotationWasDisabled){
-			enableRotation(true);
-		}
-		super.onPause();
-	}
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(
+				SystemHelper.getScreenOrientation(this));
+	}	
+	
 }
--- src/com/librelio/activity/StartupActivity.java
+++ src/com/librelio/activity/StartupActivity.java
@@ -24,12 +24,23 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
-import com.librelio.base.BaseActivity;
+import com.librelio.LibrelioApplication;
 import com.niveales.wind.R;
 
 /**
@@ -40,22 +51,33 @@ import com.niveales.wind.R;
  */
 public class StartupActivity extends AbstractLockRotationActivity {
 	private static final String TAG = "StartupActivity";
-
+	private static final String PARAM_TEST = "test";
+		
+	private static final String PARAM_CLIENT = "@client";
+	private static final String PARAM_APP = "@app";
+	
+	private ImageView startupImage;
+	
+	private boolean advertisingClickPerfomed = false; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startup);
-
+		
+		startupImage = (ImageView) findViewById(R.id.sturtup_image);
+		
 		if (hasTestMagazine()) {
-			initStorage("test");
-			new InitTestMagazines().execute("test");
+			initStorage(PARAM_TEST);
+			new InitTestMagazines().execute(PARAM_TEST);
 		} else {
-			initStorage("test");
+			initStorage(PARAM_TEST);
 			new InitPredefinedMagazinesTask().execute();
 		}
-
+		
+		new LoadAdvertisingImageTask().execute();
 	}
-
+	
 	private class InitTestMagazines extends AsyncTask<String, Void, Integer> {
 
 		@Override
@@ -117,12 +139,61 @@ public class StartupActivity extends AbstractLockRotationActivity {
 			if (isCancelled()) {
 				return;
 			}
-			onStartMagazine();
 		}
 
 	}
+	
+	private class LoadAdvertisingImageTask extends AsyncTask<Void, Void, Bitmap>{
+		@Override
+		protected Bitmap doInBackground(Void... params) {
+			
+			String imageUrl = getAdvertisingImageURL();
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpget = new HttpGet(imageUrl);
+			try {
+				HttpResponse response = httpclient.execute(httpget);
+				if (response != null){
+					HttpEntity entity = response.getEntity();
+					if (entity != null){
+						Bitmap adImage = BitmapFactory.decodeStream(entity.getContent());
+						return adImage;
+					}
+				}
+			} catch (ClientProtocolException e) {
+				Log.e(TAG, "Loading advertising image failed.", e);
+			} catch (IOException e) {
+				Log.e(TAG, "Loading advertising image failed.", e);
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap adImage) {
+			 if (adImage != null){
+				 startupImage.setImageBitmap(adImage);
+			 }
+			new LoadAdvertisingLinkTask().execute();
+		}
+	}
+	
+	private class LoadAdvertisingLinkTask extends AsyncTask<Void, Void, Void>{
+		@Override
+		protected Void doInBackground(Void... params) {
+			String linkUrl = getAdvertisingLinkURL();
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			onStartMagazine();
+		}
+	}
 
 	protected void onStartMagazine() {
+		if (advertisingClickPerfomed){
+			return;
+		}
+		
 		new Timer().schedule(new TimerTask() {
 			@Override
 			public void run() {
@@ -132,4 +203,31 @@ public class StartupActivity extends AbstractLockRotationActivity {
 			}
 		}, 1000);
 	}
+	
+	protected void onChangeRotation() {
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				setContentView(R.layout.startup);
+			}
+		}, 1000);
+	}
+	
+	private String getAdvertisingImageURL() {
+		
+		return new StringBuilder(getString(R.string.get_advertising_image_url))
+							.append(getString(R.string.get_advertising_image_end))
+							.toString()
+							.replace(PARAM_CLIENT, Uri.encode(LibrelioApplication.getClientName(this)))
+							.replace(PARAM_APP, Uri.encode(LibrelioApplication.getMagazineName(this)));
+	}
+	
+	private String getAdvertisingLinkURL() {
+		
+		return new StringBuilder(getString(R.string.get_advertising_link_url))
+							.toString()
+							.replace(PARAM_CLIENT, Uri.encode(LibrelioApplication.getClientName(this)))
+							.replace(PARAM_APP, Uri.encode(LibrelioApplication.getMagazineName(this)));
+	}
+
 }
