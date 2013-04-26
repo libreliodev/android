6c3924505491589c6f276ec55862037422abd2b9
Michael Basil <michael@intrications.com>
2013-04-26 22:43:40 +0100

    Display home as up on Action Bar in DownloadedMagazinesActivity

--- src/com/librelio/activity/DownloadedMagazinesActivity.java
+++ src/com/librelio/activity/DownloadedMagazinesActivity.java
@@ -3,6 +3,7 @@ package com.librelio.activity;
 import java.util.List;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.librelio.base.BaseActivity;
 import com.librelio.model.Magazine;
@@ -13,24 +14,33 @@ import com.niveales.wind.R;
 public class DownloadedMagazinesActivity extends BaseActivity {
 
 	private MagazineManager magazineManager;
-	
+
 	private DownloadedMagazinesListView downloadsList;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
 		setContentView(R.layout.activity_downloaded_magazines);
-		
+
 		magazineManager = new MagazineManager(this);
 
 		downloadsList = (DownloadedMagazinesListView) findViewById(R.id.activity_downloaded_magazines_list);
-		
-		List<Magazine> downloads = magazineManager.getMagazines(false, Magazine.TABLE_DOWNLOADED_MAGAZINES);
-		
+
+		List<Magazine> downloads = magazineManager.getMagazines(false,
+				Magazine.TABLE_DOWNLOADED_MAGAZINES);
+
 		downloadsList.setMagazines(downloads);
 	}
-	
-	
-	
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
