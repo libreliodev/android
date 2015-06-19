9587446f65ead037965a6401a46dc03470001220
Michael Basil <michael@intrications.com>
2015-06-19 12:59:41 +0100

    Update dependencies

--- main/src/main/java/com/librelio/view/RecyclerItemClickListener.java
+++ main/src/main/java/com/librelio/view/RecyclerItemClickListener.java
@@ -34,4 +34,9 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
   }
 
   @Override public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) { }
+
+  @Override
+  public void onRequestDisallowInterceptTouchEvent(boolean b) {
+
+  }
 }
